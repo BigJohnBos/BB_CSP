330b8fe3959df2a505d50d0e34474405bbfdd362
BigJohnBos <boston.ballash@ucas-edu.net>
2025-08-29 14:42:39 -0600

    My notes

---
+++ notes/variables.py
@@ -0,0 +1,18 @@
+# BB 7th Vairables notes
+
+# What is one way to have your program give an output?
+print("welcome to the variables notes")
+
+school = "ucas"
+single_digit = "4"
+how_much = 10
+
+print(how_much)
+
+how_much = 100
+
+print(how_much)
+
+name = input("what is your name?")
+
+print("hello, name")
