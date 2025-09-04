67406981831b9bed8c9dd0dd184db019dc8c8f57
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-04 14:41:20 -0600

    expression notes

---
+++ practices/BB_7th_expressions.py
@@ -0,0 +1,24 @@
+name = input("what is your name:\n")
+print("hello", name)
+
+apples = 12
+friends = 5
+print("each friend gets", apples/friends, "apples!")
+
+don = 7
+bob = 4
+joe = 6
+dan = 5
+
+print("the average age equals", (don + bob + joe + dan)/4)
+
+num_1 = 12
+num_2 = 7
+
+print("addition(+)", num_1+num_2 )
+print("subtraction(-)", num_1-num_2 )
+print("multiplication(*)", num_1*num_2 )
+print("division(/)", num_1/num_2 )
+print("exponents(**)", num_1**num_2 )
+print("intiger division(//)", num_1//num_2 )
+print("modulo(%)", num_1%num_2 )
