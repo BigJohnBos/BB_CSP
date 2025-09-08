ef6fe1439c4d7964b33e8c36a351e1762d2492b1
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-08 14:41:48 -0600

    9/8/25  .3

---
+++ practices/strings.py
@@ -0,0 +1,10 @@
+first_name = input("What is your first name:\n").strip().upper().title  ()
+
+last_name = input("What is your last name:\n").strip().upper().title  ()
+
+full_name = first_name + "" + last_name
+
+sentance = '    The quick brown fox jumps over the lazy dog   '
+
+print("Wlecome to my program", full_name + "!")
+print(sentance.strip())
