58c2c03fe7033afbc50bf5dd79f2860a4d860c1a
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-10 14:42:11 -0600

    my password

---
+++ my stuff/password.py
@@ -0,0 +1,18 @@
+
+
+
+user_input = input()
+password = "9812"
+
+if user_input == password:
+    print("Correct")
+else:
+    print("incorrect")
+
+user_input = input()
+password = "9812"
+
+if user_input == password:
+    print("Correct")
+else:
+    print("incorrect")
