a4eeff4f54bb0b64d116900264847c52d9b40d80
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-10 14:28:18 -0600

    name decorator and silly sentances

---
+++ practices/name_decorator.py
@@ -0,0 +1,6 @@
+# BB 7th Name decorator
+
+name = input("what is your name:\n")
+print("welcome", name)
+print("here is your decorated name")
+print("#:)" + name + "(:#")
---
+++ practices/silly_sentances.py
@@ -0,0 +1,10 @@
+# BB 7th silly sentances
+
+time = input("give me a time of day:\n")
+name = input("give me a name:\n").title()
+verb = input("give me a past tense verb:\n")
+preposition = input("give me a preposition:\n")
+noun = input("give me a noun:\n")
+
+
+print(name, "woke up in the", time, "then,", verb, preposition, "the", noun + ".")
