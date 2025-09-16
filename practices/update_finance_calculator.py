fa3adc1a074f45034721c8a588bd8e133883a246
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-16 23:42:46 +0000

    finance calculator

--- practices/update_finance_calculator.py
+++ practices/update_finance_calculator.py
@@ -4,13 +4,28 @@ def money(expense):
     return float(input(f"what is your monthly {expense}"))
 
 income = money("income")
-rent = money("rent")
-utilities = money("utilities")
-groceries = money("groceries")
-transport = money("transportation")
-savings = income*0.1
+rent = money("cost of rent")
+utilities = money("cost of utilities")
+groceries = money("cost of groceries")
+transport = money("cost of transportation")
+savings = round((income)*0.1, 2)
 
-spending_money = income - rent - utilities - groceries - transport - savings
+spending_money = round((income - rent - utilities - groceries - transport - savings), 2)
 
-print("Your spending money is $", spending_money)
+rent_percent = round((rent/income)*100, 2)
+utilities_percent = round((utilities/income)*100, 2)
+groceries_percent = round((groceries/income)*100, 2)
+transport_percent = round((transport/income)*100, 2)
+
+print(f"Your rent cost is ${rent} which is %{rent_percent} of your income")
+print(f"Your utilities cost is ${utilities} which is %{utilities_percent} of your income")
+print(f"Your groceries cost is ${groceries} which is %{groceries_percent} of your income")
+print(f"Your transportation cost is ${transport} which is %{transport_percent} of your income")
+
+
+print(f"you should put ${savings} in your savings acount which is %10 of your income")
+
+
+
+print(f"Your spending money is ${spending_money}")
 
