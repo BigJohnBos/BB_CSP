efb00730354c53345773afabd84aa487c1b6d389
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-23 14:41:00 -0600

    game

---
+++ my stuff/something.py
@@ -0,0 +1,6 @@
+name = input("Welcome what is your name: \n").capitalize()
+print(f"Welcome {name}!")
+
+print("You begin your journey at an intersection.  You can choose Left, Right, or Forward")
+direction = input("Which direction do you want to go").capitalize()
+if direction == 
