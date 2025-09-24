8cf428626396b36b847b28dc9cb4fc7d93b5bffa
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-24 14:40:39 -0600

    game

---
+++ my stuff/hangman.py
@@ -0,0 +1,9 @@
+name = input("what is your name")
+
+print(f"Hello {name}!")
+
+print("you come to a crossroad")
+print("Do you go Forward, Right, or Left")
+choice_1 = input("what way do you want to go").capitalize().strip()
+
+if choice_1 == "Forward"
