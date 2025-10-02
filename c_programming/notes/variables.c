ccfd869273c4c38f9c1767aee1b9240028c9062a
BigJohnBos <boston.ballash@ucas-edu.net>
2025-10-02 14:41:56 -0600

    bob

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,15 @@
+// BB 7th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int grade = 95; 
+    float pi = 3.14;
+    double long_pi = 3.141596258;
+    char letter_grade = 'a';
+    char name[] = "Boston";
+
+    printf("%s did it", name);
+    printf("You have a %d, in the class. that is a %c", grade, letter_grade);
+
+    return 0;
+}
