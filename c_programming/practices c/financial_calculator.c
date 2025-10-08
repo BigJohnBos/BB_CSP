d49bdd47cd428ea094dd4db2479667c4abc9e83b
BigJohnBos <boston.ballash@ucas-edu.net>
2025-10-08 14:41:44 -0600

    10/8/25

---
+++ c_programming/practices c/financial_calculator.c
@@ -0,0 +1,24 @@
+// BB 7th Financial Calculator
+
+#include <stdio.h>
+
+int main(void){
+
+    float income, transport, groceries, rent, utilities;
+
+
+
+    printf("what is your monthly income?\n");
+    scanf("%f", &income);
+    printf("What is your monthly rent/morgage\n");
+    scanf("%f", &rent)
+
+
+
+
+
+
+
+
+
+}
