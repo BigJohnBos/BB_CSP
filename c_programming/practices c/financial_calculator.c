c47546ea6f9a87b124bd03318a56cbe6cfcfa0e2
BigJohnBos <boston.ballash@ucas-edu.net>
2025-10-09 00:29:11 +0000

    bos dinner

--- c_programming/practices c/financial_calculator.c
+++ c_programming/practices c/financial_calculator.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+
 int main(void){
 
     float income, transport, groceries, rent, utilities;
@@ -11,10 +12,19 @@ int main(void){
     printf("what is your monthly income?\n");
     scanf("%f", &income);
     printf("What is your monthly rent/morgage\n");
-    scanf("%f", &rent)
+    scanf("%f", &rent);
+    printf("What is your monthly cost of utilities\n");
+    scanf("%f", &utilities);
+    printf("How much do you spend on groceries each month\n");
+    scanf("%f", &groceries);
+    printf("How much do you spend on transportation each month\n");
+    scanf("%f", &transport);
 
-
-
+    float spending_money = income - transport - groceries - rent - utilities;
+    float rent_percent = (rent/income)*100;
+    float utilities_percent = (utilities/income)*100;
+    float groceries_percent = (groceries/income)*100;
+    float transport_percent = (transport/income)*100;
 
 
 
