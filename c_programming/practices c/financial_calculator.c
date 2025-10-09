e74dd3d377b23f7dc8ad2f5ec61032de6f4695a6
BigJohnBos <boston.ballash@ucas-edu.net>
2025-10-09 01:27:08 +0000

    financial calculator c

--- c_programming/practices c/financial_calculator.c
+++ c_programming/practices c/financial_calculator.c
@@ -20,14 +20,23 @@ int main(void){
     printf("How much do you spend on transportation each month\n");
     scanf("%f", &transport);
 
-    float spending_money = income - transport - groceries - rent - utilities;
+    float savings = income*0.1;
+
+    float spending_money = income - transport - groceries - rent - utilities - savings;
     float rent_percent = (rent/income)*100;
     float utilities_percent = (utilities/income)*100;
     float groceries_percent = (groceries/income)*100;
     float transport_percent = (transport/income)*100;
 
+    printf("\nYou spent $%.2f on rent each month which is %.2f percent or your monthly income", rent, rent_percent);
+    printf("\nYou spent $%.2f on rent each month which is %.2f percent or your monthly income", utilities, utilities_percent);
+    printf("\nYou spent $%.2f on rent each month which is %.2f percent or your monthly income", groceries, groceries_percent);
+    printf("\nYou spent $%.2f on rent each month which is %.2f percent or your monthly income", transport, transport_percent);
+    printf("\nYou should put $%.2f in savings which is 10 percent of your monthly income", savings);
+    printf("\nYou have $%.2f of spending money to use each month!\n\n", spending_money);
 
 
+    return 0;
 
 
 
