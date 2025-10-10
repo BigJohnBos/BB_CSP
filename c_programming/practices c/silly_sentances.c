3d0fc62f94a8745aac08632d3bffa2187b16bf55
BigJohnBos <boston.ballash@ucas-edu.net>
2025-10-10 15:45:54 +0000

    mcdonalds was tricking you

--- c_programming/practices c/silly_sentances.c
+++ c_programming/practices c/silly_sentances.c
@@ -3,5 +3,19 @@
 #include <string.h>
 
 int main(void){
+    char noun[20];
+    char verb[20];
+    char noun2[20];
+    char animal[20];
+
+    printf("Give me an animal\n");
+    scanf("%s", animal);
+    printf("Give me a noun\n");
+    scanf("%s", noun);
+    printf("Give me a noun\n");
+    scanf("%s", noun2);
+    printf("Give me a verb\n");
+    scanf("%s", verb);
+    printf("You wake up to the smell of a %s roasting over the %s, you %s down the stairs and find that your %s was tricking you.", animal, noun, verb, noun2);
 
 }
